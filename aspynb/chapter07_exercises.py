989239eabf0439083488c4a621734b981bf0ade5
Ivan Savov <ivan.savov@gmail.com>
2025-11-02 08:37:13 -0500

    Fix solution to E7.3 -- internal demand matrix A

--- aspynb/chapter07_exercises.py
+++ aspynb/chapter07_exercises.py
@@ -15,6 +15,22 @@ def cells():
 
     '''
     ### E7.3
+    
+    A group of farmers has formed a cooperative that specializes in three crops:
+    arugula, broccoli, and carrots.
+    They need to produce 100 tons of arugula,
+    200 tons of broccoli,
+    and 300 tons of carrots for sale.
+    The farmers also need some additional production to feed themselves
+    and the work-for-food tourists that come to help them scale up production during the harvest season.
+    To feed the workers needed to grow 100 kg of arugula,
+    they need to produce an extra 10 kg of broccoli and 10 kg of carrots.
+    To produce 100 kg of broccoli,
+    it takes 1 kg of arugula and 15 kilograms of carrots.
+    To produce 100 kg of carrots,
+    it takes 1 kg of arugula
+    and 20 kilograms of broccoli.
+    How much of each vegetable should the farmers produce?
     '''
 
     '''
@@ -31,9 +47,9 @@ def cells():
 
     # The internal demands of production are describe by
     A = Matrix([
-    [    0,   0.1,   0.1],
-    [ 0.01,     0,  0.15],
-    [ 0.01,  0.20,     0]])
+    [    0,  0.01,  0.01],
+    [  0.1,     0,  0.20],
+    [  0.1,  0.15,     0]])
 
     '''
     '''
@@ -41,6 +57,10 @@ def cells():
     # The required production rates for both internal and external demands:
     x = (eye(3)-A).inv()*d
     x
+
+    '''
+    '''
+
 
     '''
     '''
